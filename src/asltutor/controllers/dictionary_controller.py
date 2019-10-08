7bfaaa05fb8a87568ba7ad116b519714d9eadd85
Richard Sweetman <rsweetman850@gmail.com>
2019-10-08 19:23:05 -0400

    Added the search and get all words endpoints

--- src/asltutor/controllers/dictionary_controller.py
+++ src/asltutor/controllers/dictionary_controller.py
@@ -2,9 +2,16 @@ from asltutor.models.dictionary import Dictionary
 from flask_mongoengine import MongoEngine
 from flask import request, Response
 from flask import Blueprint
-
+from asltutor import s3_helper
+from werkzeug import secure_filename
+from flask import render_template
 
 dictionary = Blueprint('dictionary', __name__)
+
+
+@dictionary.route('/dictionary/create')
+def create():
+    return render_template('upload.html')
 
 
 @dictionary.route('/dictionary/create', methods=['POST'])
@@ -18,18 +25,31 @@ def add_word():
 
     :rtype: None
     """
-    if request.content_type != 'application/json':
-        return Response('Failed: Content-type must be application/json', 401)
+    if 'file' not in request.files:
+        return Response('Failed: missing file', 400)
+    file = request.files['file']
 
-    r = request.get_json()
-    o = Dictionary.objects(word__contains=r['word'])
-    if o:
-        o = Dictionary.objects.get(word=r['word'])
-        for i in r:
-            o[i] = r[i]
-        o.save()
+    r = request.form.to_dict()
+    """
+        These attributes are also available
+
+        file.filename
+        file.content_type
+        file.content_length
+        file.mimetype
+
+    """
+    # TODO need to have admin validate the words
+    if file:
+        file.filename = secure_filename(file.filename)
+        output = s3_helper.upload_file_to_s3(file)
+        try:
+            Dictionary(word=r['word'], url=output, in_dictionary=True).save()
+        except Exception as e:
+            print(e)
+            return Response('Failed: error', 500)
     else:
-        Dictionary(**r).save()
+        return redirect('/dictionary/create')
     return Response('Success', 200)
 
 
@@ -45,24 +65,48 @@ def delete_word():
     :rtype: None
     """
     input_ = request.args.get('input')
-    Dictionary.objects.get_or_404(word=input_).delete()
-    return Response('Success', 200)
+    input_ = input_.lower()
+    if Dictionary.objects(word=input_).delete():
+        return Response('Success: word deleted from the dictionary', 200)
+    return Response('Word not found', 204)
 
 
-@dictionary.route('/dictionary', methods=['GET'])
-def get_word():
+@dictionary.route('/dictionary/<string:word>', methods=['GET'])
+def get_word(word):
     """Get a word in the dictionary
 
     Returns a JSON response contianing the word document of the word requested
 
-    query parameter: /dictionary?input=someword
+    path parameter: /dictionary/word
     no request body
 
     :rtype: JSON
     """
-    input_ = request.args.get('input')
-    o = Dictionary.objects.get_or_404(word=input_)
+    word = ''.join(filter(str.isalpha, word)).lower()
+    print(word)
+    if not Dictionary.objects(word=word):
+        Dictionary(word=word, times_requested=1).save()
+        return Response('Word not found', 204)
+
+    o = Dictionary.objects.get(word=word)
     if o.in_dictionary == False:
-        Dictionary.objects(word=input_).update_one(
+        Dictionary.objects(word=word).update_one(
             upsert=True, inc__times_requested=1)
+        return Response('Word not found', 204)
+
     return Response(o.to_json(), 200, mimetype='application/json')
+
+
+@dictionary.route('/dictionary', methods=['GET'])
+def get_words():
+    return Response(Dictionary.objects(in_dictionary=True).only('word').to_json(), 200, mimetype='application/json')
+
+
+@dictionary.route('/dictionary/search', methods=['GET'])
+def search_word():
+    input_ = request.args.get('input')
+    input_ = input_.lower()
+    o = Dictionary.objects(word__contains=input_)
+    if o:
+        return Response(o.to_json(), 200, mimetype='application/json')
+    return Response('Word not found', 204)
