d700c18d1f35e6176be08be13e6d9c973eaf1976
Richard Sweetman <rsweetman850@gmail.com>
2019-10-18 14:43:19 -0400

    Adjusted the settings

--- src/asltutor/settings.py
+++ src/asltutor/settings.py
@@ -4,16 +4,16 @@ import os
 class BaseConfig(object):
     DEBUG = False
     TESTING = False
+    MONGODB_SETTINGS = {
+        'db': 'asl',
+        'host': 'mongodb://localhost:27017/asl_tutor'
+    }
 
 
 class DevelopmentConfig(BaseConfig):
     DEBUG = True
     TESTING = True
     ENV = 'development'
-    MONGODB_SETTINGS = {
-        'db': 'asl',
-        'host': 'mongodb://localhost:27017/asl_tutor'
-    }
     S3_BUCKET = os.environ.get("S3_BUCKET")
     S3_KEY = os.environ.get("S3_KEY")
     S3_SECRET_ACCESS_KEY = os.environ.get("S3_SECRET_ACCESS_KEY")
@@ -22,10 +22,6 @@ class DevelopmentConfig(BaseConfig):
 
 class ProductionConfig(BaseConfig):
     SECRET_KEY = os.environ.get("SECRET_KEY")
-    MONGODB_SETTINGS = {
-        'db': 'asl',
-        'host': 'mongodb://localhost:27017/asl_tutor'
-    }
     S3_BUCKET = os.environ.get("S3_BUCKET")
     S3_KEY = os.environ.get("S3_KEY")
     S3_SECRET_ACCESS_KEY = os.environ.get("S3_SECRET_ACCESS_KEY")
